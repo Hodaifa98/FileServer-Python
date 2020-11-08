ba6014d254b0f0c788cbb5369e0736084d8fc499
Hodaifa98 <40869908+Hodaifa98@users.noreply.github.com>
2020-11-08 23:54:52 +0100

    Added getFile method.

--- file_server.py
+++ file_server.py
@@ -3,3 +3,19 @@ import os
 import socket
 import threading
 
+def getFile(name: str, s: socket):
+    file_name = s.recv(1024)
+    if os.path.isfile(file_name):
+        size = str(os.path.getsize(file_name))
+        s.send(f"EXISTS {size}")
+        user_response = s.recv(1024)
+        if user_response[::2] == "OK":
+            with open(file_name, "rb") as f:
+                bytes_to_send = f.read(1024)
+                s.send(bytes_to_send)
+                while bytes_to_send != "":
+                    bytes_to_send = f.read(1024)
+                    s.send(bytes_to_send)
+    else:
+        s.send("ERR")
+    s.close()
